ff5cfbeec8fc6d93fc7e2030b1124af60e641f34
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-19 23:00:43 +0530

    Update Simple_Interest.java

--- Simple_Interest.java
+++ Simple_Interest.java
@@ -2,18 +2,25 @@
 import java.util.*;
 class Simple_Interest
 {
+    // main method
     public static void main(String[] args)
     {
         double p,r,t,si;
         Scanner sc = new Scanner(System.in);
+        
+        // taking principal amount from user
         System.out.print("Please enter the Pricipal Amount : Rs ");
         p = sc.nextDouble();
         
+        // taking rate of interset from user
         System.out.print("Please enter the Rate of Interest : % ");
         r = sc.nextDouble();
         
+        // taking time period from user
         System.out.print("Please enter the Time period in Years :  ");
         t = sc.nextDouble();
+        
+        // formula of simple interset
         si = p*(1+(r*t));
         
         System.out.println("Simple Interest is : "+si);
