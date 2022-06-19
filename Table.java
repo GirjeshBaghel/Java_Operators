01790361bc9ebdc988449d145783e911b1084a2a
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-19 23:04:20 +0530

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,14 +1,18 @@
 import java.util.*;
 class Table
 {
+    // main method
     public static void main(String[] args)
     {
         int a;
         Scanner sc = new Scanner(System.in);
+
+        // which no of table you wants to print
         System.out.print("Please enter the no : ");
         a = sc.nextInt();
         for(int i=1; i<11;i++)
         {
+            // pattern to print table
             System.out.println(a+" * "+i+" = "+(a*i));
         }
 
