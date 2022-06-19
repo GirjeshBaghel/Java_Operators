ae1632f7df599cfc31e30af7eeaa282a175174c0
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-19 22:58:11 +0530

    Update Profit_Loss.java

--- Profit_Loss.java
+++ Profit_Loss.java
@@ -2,16 +2,21 @@
 import java.util.*;
 class Profit_Loss
 {
+    // main method
     public static void main(String[] args)
     {
         int sp,cp;
         Scanner sc = new Scanner(System.in);
+        
+        // taking selling price from user
         System.out.print("Enter the Selling Price here : ");
         sp = sc.nextInt();
 
+        // taking cost price from user
         System.out.print("Enter the Cost Price here : ");
         cp = sc.nextInt();
-
+        
+        // checking selling is greater or not
         if(sp>cp)
         {
             System.out.println("Profit by : "+(sp-cp));
