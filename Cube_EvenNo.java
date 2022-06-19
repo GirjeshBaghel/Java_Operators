696f9a28a27c0978e30d5c8c7f1d3ef1c49cb777
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-19 12:07:25 +0530

    Update Cube_EvenNo.java

--- Cube_EvenNo.java
+++ Cube_EvenNo.java
@@ -5,18 +5,25 @@ class Cube_EvenNo
     public static void main(String[] args)
     {
         int a;
+        
+        // taking a no from user
         System.out.print("Enter a no : ");
         Scanner sc = new Scanner(System.in);
         a = sc.nextInt();
+        
+        // checking no will not be zero
         if(a == 0)
         {
             System.out.println("The no is neither positive nor negative.. Please Enter the corrext value");
         }
-
+        
+        // checking no is even or not, if even them print cube of that no
         else if(a%2 == 0)
         {
             System.out.println("The value is even and Cude of no is : "+a*a*a);
         }
+        
+        // if no is not even then print this line
         else{
             System.out.println("The value is negative value please enter the correct value");
         }
