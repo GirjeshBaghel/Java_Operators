fae829dac4d0bcff9d39946172c97b54096fcbf2
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-19 12:09:17 +0530

    Update Locical_Operator.java

--- Locical_Operator.java
+++ Locical_Operator.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 class Locical_Operator
 {
+    // mmain method
     public static void main(String[] args)
     {
             int a,b;
+            // taking a no from user
             System.out.print("Enter the two no");
             Scanner sc = new Scanner(System.in);
             a = sc.nextInt();
             b = sc.nextInt();
             
+            // printing the value of that two no in and,or,not operator
             System.out.println("AND : "+(a>b && b<a));
 
             System.out.println("OR : "+(a<b || b<a));
