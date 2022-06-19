8df4f06de1fe2908ddcbebfde2f310280aa19e92
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-19 12:00:11 +0530

    Update Area_Rectange.java

--- Area_Rectange.java
+++ Area_Rectange.java
@@ -5,14 +5,19 @@ class Area_Rectange
     public static void main(String[] args)
     {
         double l,b,area,peri;
+        // taking the length from user
         System.out.print("Enter the Lenth of Rectangle : ");
         Scanner sc = new Scanner(System.in);
         l = sc.nextDouble();
         
+        // taking the breath from user
         System.out.print("Enter the Breath of Rectangle : ");
         b = sc.nextDouble();
         
+        // formula of area of reactangle
         area = l*b;
+        
+        // formula of permiter of rectangle
         peri = 2*(l+b);
         System.out.println("Area of Rectangle is : "+area);
         System.out.println("Perimeter of Rectangle is : "+peri);
