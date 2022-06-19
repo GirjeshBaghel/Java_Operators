728c73a5a0283ecd6f171f7bde9a37493b3fad92
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-19 11:56:55 +0530

    Update Area_Circle.java

--- Area_Circle.java
+++ Area_Circle.java
@@ -5,11 +5,16 @@ class Area_Circle
     public static void main(String[] args)
     {
             double rad,area,cir,pie=3.14;
-            System.out.print("Enter the Radius of circle to find the Area of circle : ");
+           
+        // taking radius from user
+        System.out.print("Enter the Radius of circle to find the Area of circle : ");
             Scanner sc = new Scanner(System.in);
             r = sc.nextDouble();
+        // formula of area of circle 
             area = pie*(rad*rad);
+        // formula of circumfrances of circle
             cir = 2*(pie*rad);
+        
             System.out.println("Area of Circle is : "+area);
             System.out.println("Circumference of Circle is :  "+cir);
     }
